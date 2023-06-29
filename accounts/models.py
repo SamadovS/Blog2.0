471c0da639bbd14488b7b4f2a3b643909f1cf859
SamadovS <siroj.origin.profile@gmail.com>
2023-06-29 14:46:28 +0900

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
